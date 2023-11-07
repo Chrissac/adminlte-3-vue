3778c5fc2b1ebaf7618f9bfa05f5b36e3cf37b03
Christopher Sacripante <csacripante@hadrian-inc.com>
2023-11-07 16:33:13 -0500

    changed ts file on available games

--- src/pages/availableGames/availableGames.ts
+++ src/pages/availableGames/availableGames.ts
@@ -4,53 +4,39 @@ import { defineComponent, ref, onMounted } from 'vue';
 import { IGames } from '@/interfaces/gamesList';
 import { getAllAvailableGames } from '@/services/gamesAuth';
 import { Goalies } from '@/interfaces/goalies';
-interface GoalieData {
-  displayName: string;
-  email: string;
-  phone: string;
-  // Add other fields if needed
-}
-export default defineComponent({
-  setup() {
-    const toast = useToast();
-    const games = ref([] as IGames[]);
-    const expandedRows = ref([] as Goalies[]);
-    const loading = ref(false);
-    const filters = ref({
-      global: { value: '', matchMode: 'contains' },
-    });
 
-
-    function confirmGoalie(goalieData: GoalieData) {
-      const confirmationMessage = `Confirmed goalie: ${goalieData.displayName}`;
-      toast.add({ severity: 'success', summary: 'Confirmation', detail: confirmationMessage });
-    }
-
-    function exportCSV() {
-      // This is not a type error because `this.$refs.dt` is likely to be defined in the component's template
-      // If it's not defined, it will throw a runtime error
-      this.$refs.dt?.exportCSV();
-    }
-
-    const fetchAvailableGames = async () => {
-      loading.value = true;
-      games.value = (await getAllAvailableGames()).data;
-      expandedRows.value = null;
-      loading.value = false;
-    };
-
-    onMounted(() => {
-      // Fetch data when the component is mounted
-      fetchAvailableGames();
-    });
-    
-
+export default {
+  data() {
     return {
-      games,
-      loading,
-      filters,
-      exportCSV,
-      expandedRows,
+      games: [] as IGames[],
+      expandedRows: [] as Goalies[],
+      loading: false,
+      filters: {
+        global: { value: '', matchMode: 'contains' },
+      },
     };
   },
-});
+  methods: {
+    confirmGoalie(goalieData: Goalies, index : number) {
+      if (goalieData) {
+        const confirmationMessage = `Confirmed goalie: ${goalieData.displayName}`;
+        console.log(confirmationMessage);
+        this.$toast.add({ severity: 'success', summary: 'Confirmation', detail: confirmationMessage });
+      } else {
+        console.error('No goalie data available.');
+      }
+    },
+    exportCSV() {
+      this.$refs.dt?.exportCSV();
+    },
+    async fetchAvailableGames() {
+      this.loading = true;
+      this.games = (await getAllAvailableGames()).data;
+      this.expandedRows = null; // You can remove this line
+      this.loading = false;
+    },
+  },
+  async mounted() {
+    await this.fetchAvailableGames();
+  },
+};
